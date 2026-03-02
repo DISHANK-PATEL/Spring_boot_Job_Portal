a23e27409c79a8e8b3cf89e2172300c0dfc5d13a
DYNAMIC_DISHANK <dishankpatel16082003@gmail.com>
2026-03-02 16:46:41 +0530

    Added caching using caffeinne with TTL

--- Job_portal_backend/Project/jobportal/src/main/java/com/dishank/jobportal/JobportalApplication.java
+++ Job_portal_backend/Project/jobportal/src/main/java/com/dishank/jobportal/JobportalApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class JobportalApplication {
